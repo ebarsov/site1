a00d1a1da1a1fae04e68accf8809c2b90cb52982
ebarsov <109013764+ebarsov@users.noreply.github.com>
2022-07-26 12:54:22 -0700

    Add files via upload

---
+++ first-theme/footer.php
@@ -0,0 +1,29 @@
+<footer>
+<div class="inner-footer">
+<h2>Tours of the Month!</h2>
+
+<?php dynamic_sidebar('sidebar-footer'); ?>
+</div>
+
+<div class="inner-footer">
+<ul>
+<li>Copyright &copy; <?php echo date('Y') ; ?></li>
+<li>All Rights Reserved</li>
+<li><a href="">Web Design by Elena</a></li>
+<li><a href="">Valid HTML</a></li>
+<li><a href="">Valid CSS</a></li>
+</ul>
+</div>
+
+
+
+</footer>
+<script>
+		$(document).ready(function(){
+			$(".nav-button").click(function () {
+			$(".nav-button,.primary-nav").toggleClass("open");
+			});    
+		});
+		</script>
+</body>
+</html>
